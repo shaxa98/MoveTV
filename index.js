ea42f4698dd072c0d8f82a1dd64167aeb3465df6
Bekhzod96 <mr.begzodjon96@mail.ru>
2023-04-09 13:17:40 +0300

    register added

--- index.js
+++ index.js
@@ -3,33 +3,20 @@ const registerForm = document.getElementById('regisration-form');
 const registerSuccessBanner = document.getElementById(
   'regisration-success-bunner'
 );
+
 const users = [];
 
-// const user = {
-//   username: '',
-//   email: '',
-//   password: '',
-// };
-
 const username = document.getElementById('username');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-function register(username,password){
-  username = user 
-    password = pas
-   return('You are successfully registrated',username);
-   }
-function login(user,pas){
-  if(user===username && pas===pasword ) { 
-  return('You are welcome',username);
-    } 
-  else {
-    return('Wrong creadentials ')
-  }
-}
-
-
 registerSubmitBtn.onclick = () => {
+  const user = {
+    username: username.value,
+    email: email.value,
+    password: password.value,
+  };
+  users.push(user);
   registerForm.classList.add('d-none');
   registerSuccessBanner.classList.remove('d-none');
 };

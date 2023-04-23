2504a8083f8addc1f9d092824fbaf1b69be031e5
Bekhzod96 <mr.begzodjon96@mail.ru>
2023-04-23 14:01:32 +0300

    movie added

--- movies.js
+++ movies.js
@@ -1,4 +1,59 @@
 // fetch
+const movie = {
+  id: 3,
+  url: "https://www.tvmaze.com/shows/3/bitten",
+  name: "Bitten",
+  type: "Scripted",
+  language: "English",
+  genres: ["Drama", "Horror", "Romance"],
+  status: "Ended",
+  runtime: 60,
+  averageRuntime: 60,
+  premiered: "2014-01-11",
+  ended: "2016-04-15",
+  officialSite: "http://bitten.space.ca/",
+  schedule: {
+    time: "22:00",
+    days: ["Friday"],
+  },
+  rating: {
+    average: 7.5,
+  },
+  weight: 94,
+  network: {
+    id: 7,
+    name: "CTV Sci-Fi Channel",
+    country: {
+      name: "Canada",
+      code: "CA",
+      timezone: "America/Halifax",
+    },
+    officialSite: "https://www.ctv.ca/sci-fi",
+  },
+  webChannel: null,
+  dvdCountry: null,
+  externals: {
+    tvrage: 34965,
+    thetvdb: 269550,
+    imdb: "tt2365946",
+  },
+  image: {
+    medium: "https://static.tvmaze.com/uploads/images/medium_portrait/0/15.jpg",
+    original:
+      "https://static.tvmaze.com/uploads/images/original_untouched/0/15.jpg",
+  },
+  summary:
+    '<p>Based on the critically acclaimed series of novels from Kelley Armstrong. Set in Toronto and upper New York State, <b>Bitten</b> follows the adventures of 28-year-old Elena Michaels, the world\'s only female werewolf. An orphan, Elena thought she finally found her "happily ever after" with her new love Clayton, until her life changed forever. With one small bite, the normal life she craved was taken away and she was left to survive life with the Pack.</p>',
+  updated: 1665307838,
+  _links: {
+    self: {
+      href: "https://api.tvmaze.com/shows/3",
+    },
+    previousepisode: {
+      href: "https://api.tvmaze.com/episodes/631862",
+    },
+  },
+};
 
 // const movies = [
 //   {

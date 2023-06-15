e51061657ca2793d66352fd3f6d579d4b26deba8
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-06-15 20:10:28 +0500

    add setTimeout function

--- person.js
+++ person.js
@@ -1,340 +1,331 @@
-let people = [
-  {
-    id: 1,
-    url: "https://www.tvmaze.com/people/1/mike-vogel",
-    name: "Mike Vogel",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1979-07-17",
-    deathday: null,
-    gender: "Male",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/0/1815.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/0/1815.jpg",
-    },
-    updated: 1672996558,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/1",
-      },
-    },
-  },
-  {
-    id: 2,
-    url: "https://www.tvmaze.com/people/2/rachelle-lefevre",
-    name: "Rachelle Lefevre",
-    country: {
-      name: "Canada",
-      code: "CA",
-      timezone: "America/Halifax",
-    },
-    birthday: "1979-02-01",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/82/207417.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/82/207417.jpg",
-    },
-    updated: 1686336048,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/2",
-      },
-    },
-  },
-  {
-    id: 3,
-    url: "https://www.tvmaze.com/people/3/alexander-koch",
-    name: "Alexander Koch",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1988-02-24",
-    deathday: null,
-    gender: "Male",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/205/513325.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/205/513325.jpg",
-    },
-    updated: 1651887118,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/3",
-      },
-    },
-  },
-  {
-    id: 4,
-    url: "https://www.tvmaze.com/people/4/eddie-cahill",
-    name: "Eddie Cahill",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1978-01-15",
-    deathday: null,
-    gender: "Male",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/0/1162.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/0/1162.jpg",
-    },
-    updated: 1678035346,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/4",
-      },
-    },
-  },
-  {
-    id: 5,
-    url: "https://www.tvmaze.com/people/5/colin-ford",
-    name: "Colin Ford",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1996-09-12",
-    deathday: null,
-    gender: "Male",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/142/356748.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/142/356748.jpg",
-    },
-    updated: 1683411102,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/5",
-      },
-    },
-  },
-  {
-    id: 6,
-    url: "https://www.tvmaze.com/people/6/nicholas-strong",
-    name: "Nicholas Strong",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: null,
-    deathday: null,
-    gender: "Male",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/0/2499.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/0/2499.jpg",
-    },
-    updated: 1561568499,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/6",
-      },
-    },
-  },
-  {
-    id: 7,
-    url: "https://www.tvmaze.com/people/7/mackenzie-lintz",
-    name: "Mackenzie Lintz",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1996-11-22",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/3/7816.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/3/7816.jpg",
-    },
-    updated: 1439593087,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/7",
-      },
-    },
-  },
-  {
-    id: 8,
-    url: "https://www.tvmaze.com/people/8/karla-crome",
-    name: "Karla Crome",
-    country: {
-      name: "United Kingdom",
-      code: "GB",
-      timezone: "Europe/London",
-    },
-    birthday: "1988-06-22",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/3/7817.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/3/7817.jpg",
-    },
-    updated: 1679018480,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/8",
-      },
-    },
-  },
-  {
-    id: 9,
-    url: "https://www.tvmaze.com/people/9/dean-norris",
-    name: "Dean Norris",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1963-04-08",
-    deathday: null,
-    gender: "Male",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/461/1154757.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/461/1154757.jpg",
-    },
-    updated: 1685800384,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/9",
-      },
-    },
-  },
-  {
-    id: 10,
-    url: "https://www.tvmaze.com/people/10/natalie-martinez",
-    name: "Natalie Martinez",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1984-07-12",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/282/706093.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/282/706093.jpg",
-    },
-    updated: 1647563298,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/10",
-      },
-    },
-  },
-  {
-    id: 11,
-    url: "https://www.tvmaze.com/people/11/britt-robertson",
-    name: "Britt Robertson",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1990-04-18",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/158/396055.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/158/396055.jpg",
-    },
-    updated: 1682709018,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/11",
-      },
-    },
-  },
-  {
-    id: 12,
-    url: "https://www.tvmaze.com/people/12/aisha-hinds",
-    name: "Aisha Hinds",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1975-11-13",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/1/2665.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/1/2665.jpg",
-    },
-    updated: 1685557497,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/12",
-      },
-    },
-  },
-  {
-    id: 13,
-    url: "https://www.tvmaze.com/people/13/jolene-purdy",
-    name: "Jolene Purdy",
-    country: {
-      name: "United States",
-      code: "US",
-      timezone: "America/New_York",
-    },
-    birthday: "1983-12-09",
-    deathday: null,
-    gender: "Female",
-    image: {
-      medium:
-        "https://static.tvmaze.com/uploads/images/medium_portrait/2/5993.jpg",
-      original:
-        "https://static.tvmaze.com/uploads/images/original_untouched/2/5993.jpg",
-    },
-    updated: 1671566347,
-    _links: {
-      self: {
-        href: "https://api.tvmaze.com/people/13",
-      },
-    },
-  },
-];
-//let people = "";
-
-fetch("https://api.tvmaze.com/people")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    people += data;
-    //personsHTML = data;
-    //makePersonContainer();
-  });
+// let people = [
+//   {
+//     id: 1,
+//     url: "https://www.tvmaze.com/people/1/mike-vogel",
+//     name: "Mike Vogel",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1979-07-17",
+//     deathday: null,
+//     gender: "Male",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/0/1815.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/0/1815.jpg",
+//     },
+//     updated: 1672996558,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/1",
+//       },
+//     },
+//   },
+//   {
+//     id: 2,
+//     url: "https://www.tvmaze.com/people/2/rachelle-lefevre",
+//     name: "Rachelle Lefevre",
+//     country: {
+//       name: "Canada",
+//       code: "CA",
+//       timezone: "America/Halifax",
+//     },
+//     birthday: "1979-02-01",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/82/207417.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/82/207417.jpg",
+//     },
+//     updated: 1686336048,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/2",
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     url: "https://www.tvmaze.com/people/3/alexander-koch",
+//     name: "Alexander Koch",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1988-02-24",
+//     deathday: null,
+//     gender: "Male",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/205/513325.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/205/513325.jpg",
+//     },
+//     updated: 1651887118,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/3",
+//       },
+//     },
+//   },
+//   {
+//     id: 4,
+//     url: "https://www.tvmaze.com/people/4/eddie-cahill",
+//     name: "Eddie Cahill",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1978-01-15",
+//     deathday: null,
+//     gender: "Male",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/0/1162.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/0/1162.jpg",
+//     },
+//     updated: 1678035346,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/4",
+//       },
+//     },
+//   },
+//   {
+//     id: 5,
+//     url: "https://www.tvmaze.com/people/5/colin-ford",
+//     name: "Colin Ford",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1996-09-12",
+//     deathday: null,
+//     gender: "Male",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/142/356748.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/142/356748.jpg",
+//     },
+//     updated: 1683411102,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/5",
+//       },
+//     },
+//   },
+//   {
+//     id: 6,
+//     url: "https://www.tvmaze.com/people/6/nicholas-strong",
+//     name: "Nicholas Strong",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: null,
+//     deathday: null,
+//     gender: "Male",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/0/2499.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/0/2499.jpg",
+//     },
+//     updated: 1561568499,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/6",
+//       },
+//     },
+//   },
+//   {
+//     id: 7,
+//     url: "https://www.tvmaze.com/people/7/mackenzie-lintz",
+//     name: "Mackenzie Lintz",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1996-11-22",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/3/7816.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/3/7816.jpg",
+//     },
+//     updated: 1439593087,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/7",
+//       },
+//     },
+//   },
+//   {
+//     id: 8,
+//     url: "https://www.tvmaze.com/people/8/karla-crome",
+//     name: "Karla Crome",
+//     country: {
+//       name: "United Kingdom",
+//       code: "GB",
+//       timezone: "Europe/London",
+//     },
+//     birthday: "1988-06-22",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/3/7817.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/3/7817.jpg",
+//     },
+//     updated: 1679018480,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/8",
+//       },
+//     },
+//   },
+//   {
+//     id: 9,
+//     url: "https://www.tvmaze.com/people/9/dean-norris",
+//     name: "Dean Norris",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1963-04-08",
+//     deathday: null,
+//     gender: "Male",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/461/1154757.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/461/1154757.jpg",
+//     },
+//     updated: 1685800384,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/9",
+//       },
+//     },
+//   },
+//   {
+//     id: 10,
+//     url: "https://www.tvmaze.com/people/10/natalie-martinez",
+//     name: "Natalie Martinez",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1984-07-12",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/282/706093.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/282/706093.jpg",
+//     },
+//     updated: 1647563298,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/10",
+//       },
+//     },
+//   },
+//   {
+//     id: 11,
+//     url: "https://www.tvmaze.com/people/11/britt-robertson",
+//     name: "Britt Robertson",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1990-04-18",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/158/396055.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/158/396055.jpg",
+//     },
+//     updated: 1682709018,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/11",
+//       },
+//     },
+//   },
+//   {
+//     id: 12,
+//     url: "https://www.tvmaze.com/people/12/aisha-hinds",
+//     name: "Aisha Hinds",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1975-11-13",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/1/2665.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/1/2665.jpg",
+//     },
+//     updated: 1685557497,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/12",
+//       },
+//     },
+//   },
+//   {
+//     id: 13,
+//     url: "https://www.tvmaze.com/people/13/jolene-purdy",
+//     name: "Jolene Purdy",
+//     country: {
+//       name: "United States",
+//       code: "US",
+//       timezone: "America/New_York",
+//     },
+//     birthday: "1983-12-09",
+//     deathday: null,
+//     gender: "Female",
+//     image: {
+//       medium:
+//         "https://static.tvmaze.com/uploads/images/medium_portrait/2/5993.jpg",
+//       original:
+//         "https://static.tvmaze.com/uploads/images/original_untouched/2/5993.jpg",
+//     },
+//     updated: 1671566347,
+//     _links: {
+//       self: {
+//         href: "https://api.tvmaze.com/people/13",
+//       },
+//     },
+//   },
+// ];
+let people = [];
 
 const personKard = (img, Pname) => {
   return ` <div class="card">
@@ -354,11 +345,23 @@ function makePersonContainer() {
   let personContainer = document.getElementById("people-container");
   let personsHTML = "";
   for (const peoples of people) {
-    const personHTML = personKard(peoples.image.medium, peoples.name);
+    const img =
+      peoples.image?.medium ||
+      "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
+    const personHTML = personKard(img, peoples.name);
     personsHTML += personHTML;
   }
   personContainer.innerHTML = personsHTML;
   //console.log(personsHTML);
 }
 
-makePersonContainer();
+fetch("https://api.tvmaze.com/people")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    people = data;
+    setTimeout(makePersonContainer, 1500);
+
+    //personsHTML = data;
+    //makePersonContainer();
+  });
